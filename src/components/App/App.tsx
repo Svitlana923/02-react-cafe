481bbf8d1b3fed30d72c746bf549c90ce71cc78f
Svitlana923 <svitlanakostiuk11@gmail.com>
2025-08-31 23:58:13 +0200

    with buttons

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,44 @@
 
 import css from './App.module.css'
 import CafeInfo from '../CafeInfo/CafeInfo'
+import VoteOptions from '../VoteOptions/VoteOptions'
+import VoteStats from '../VoteStats/VoteStats'
+import Votes, { VoteType } from '../../types/votes' 
+import { useState } from "react";
 
 function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  })
+
+  function handleVote(voteType: VoteType) {
+    setVotes(prevVotes => ({
+      ...prevVotes,
+      [voteType]: prevVotes[voteType] + 1,
+    }))
+  }
+
+  function resetVotes() {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    })
+  }
 
   return (
-      <div className={css.app}>
-        <CafeInfo />
+    <div className={css.app}>
+      <CafeInfo />
+
+      <VoteOptions 
+        onVote={handleVote} 
+        onReset={resetVotes} 
+        canReset={true} 
+      />
+
+
     </div>
   )
 }
